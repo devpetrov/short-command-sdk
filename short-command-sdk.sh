420f5020279a994d73a7e111a683d2e4d97190e9
Pavel Petrov <841636+devpetrov@users.noreply.github.com>
2021-04-29 23:54:25 +0300

    Export installation directory and set version build time.
    According to the new way installation directory is determined in Shoco
    export the build dir as installation directory when sourcing.
    
    Set the version to 'dev' on build.

--- short-command-sdk.sh
+++ short-command-sdk.sh
@@ -18,18 +18,24 @@ short-command-sdk ()
     local OPTIND
     local OPTARG
     local OPTION
-    while getopts "bs" OPTION; do
+
+    local BUILD_VERSION
+    local OUTPUT_DIR
+
+    while getopts "bd:sv:" OPTION; do
         case ${OPTION} in
             b) DO_BUILD=1;;
+            d) OUTPUT_DIR="$OPTARG";;
             s) DO_LOAD=1;;
+            v) BUILD_VERSION="$OPTARG";;
         esac
     done
 
     local SDK_DIR="$( cd "$(dirname ${BASH_SOURCE[0]} )" >/dev/null 2>&1 && pwd )"
-    local BUILDS_DIR="$SDK_DIR/builds"
-    local OUTPUT_DIR_NAME="dev"
+    BUILD_VERSION="${BUILD_VERSION:-dev}"
+    OUTPUT_DIR="${OUTPUT_DIR:-$SDK_DIR/builds}"
+    OUTPUT_DIR="$OUTPUT_DIR/$BUILD_VERSION"
 
-    local OUTPUT_DIR="$BUILDS_DIR/$OUTPUT_DIR_NAME"
     local OUTPUT_FILE="$OUTPUT_DIR/shoco.sh"
     local OUTPUT_HELPS_DIR="$OUTPUT_DIR/helps"
 
@@ -43,13 +49,14 @@ short-command-sdk ()
     fi
 
     if [ 1 -eq "$DO_LOAD" ]; then
-        _short-command-sdk-source "$OUTPUT_FILE"
+        _short-command-sdk-source "$OUTPUT_DIR" "$OUTPUT_FILE"
     fi
 }
 
 _short-command-sdk-source ()
 {
-    . "$1"
+    export SHOCO_INSTALLATION_DIR="$(dirname $1)"
+    . "$2"
 
     if [ 0 -eq "$?" ]; then
         printf "Build sourced in the current shell.\n"
@@ -103,6 +110,7 @@ _short-command-sdk-build ()
     cp $SHOCO_SDK_CFG_PROJECT_DIR/$SHOCO_SDK_CFG_LICENSE_FILE $OUTPUT_HELPS_DIR/license
 
     sed -i "s/___LATEST_VERSION_DATA_URL___/${SHOCO_SDK_CFG_LATEST_VERSION_DATA_URL//\//\\/}/" $OUTPUT_FILE
+    sed -i "s/___VERSION___/${BUILD_VERSION}/" $OUTPUT_FILE
 
     printf "Build done.\n"
 }
